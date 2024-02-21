b16b44d11a99bb00ec3bf4c185332f323afc872c
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-02-21 22:39:09 +0000

    user tests and json store

--- src/models/db.js
+++ src/models/db.js
@@ -2,13 +2,15 @@ import { userMemStore } from "./mem/user-mem-store.js";
 import { placemarkMemStore } from "./mem/placemark-mem-store.js";
 import { siteMemStore } from "./mem/site-mem-store.js";
 
+import { userJsonStore } from "./json/user-json-store.js";
+
 export const db = {
     userStore: null,
     placemarkStore: null,
     siteStore: null,
 
     init() {
-        this.userStore = userMemStore;
+        this.userStore = userJsonStore;
         this.placemarkStore = placemarkMemStore;
         this.siteStore = siteMemStore;
     },
