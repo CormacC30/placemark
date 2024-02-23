9ab7e7610251978906ae6e858326cc1e91acedae
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-02-23 11:00:35 +0000

    placemark API and tests configured

--- src/api-routes.js
+++ src/api-routes.js
@@ -1,8 +1,14 @@
 import { userApi } from "./api/user-api.js";
+import { placemarkApi } from "./api/placemark-api.js";
 
 export const apiRoutes = [
   { method: "POST", path: "/api/users", config: userApi.create },
   { method: "GET", path: "/api/users", config: userApi.find },
   { method: "DELETE", path: "/api/users", config: userApi.deleteAll },
   { method: "GET", path: "/api/users/{id}", config: userApi.findOne },
+  { method: "GET", path: "/api/placemarks", config: placemarkApi.find },
+  { method: "POST", path: "/api/placemarks", config: placemarkApi.create },
+  { method: "DELETE", path: "/api/placemarks/{id}", config: placemarkApi.deleteOne },
+  { method: "DELETE", path: "/api/placemarks", config: placemarkApi.deleteAll },
+  { method: "GET", path: "/api/placemarks/{id}", config: placemarkApi.findOne },
 ];
--- test/api/placemark-service.js
+++ test/api/placemark-service.js
@@ -23,4 +23,29 @@ export const placemarkService = {
     const res = await axios.delete(`${this.placemarkUrl}/api/users`);
     return res.data;
   },
+
+  async createPlacemark(placemark) {
+    const res = await axios.post(`${this.placemarkUrl}/api/placemarks`, placemark);
+    return res.data;
+  },
+
+  async getPlacemark(id) {
+    const res = await axios.get(`${this.placemarkUrl}/api/placemarks/${id}`);
+    return res.data;
+  },
+
+  async getAllPlacemarks() {
+    const res = await axios.get(`${this.placemarkUrl}/api/placemarks`);
+    return res.data;
+  },
+
+  async deleteAllPlacemarks() {
+    const res = await axios.delete(`${this.placemarkUrl}/api/placemarks`);
+    return res.data;
+  },
+
+  async deletePlacemark(id) {
+    const res = await axios.delete(`${this.placemarkUrl}/api/placemarks/${id}`);
+    return res;
+  },
 };
