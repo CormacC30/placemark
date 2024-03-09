3cc272993543687c57c5a43db3dc87813fff5b21
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2024-03-09 13:58:47 +0000

    seeded placemark and site data to db

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -20,4 +20,23 @@ export const seedData = {
         password: "secret"
       }
     },
+    placemarks: {
+      _model: "Placemark",
+      schull: {
+        name: "schull",
+        category: "Bronze Age",
+        userid: "->users.homer"
+      },
+    },
+    sites: {
+      _model: "Site",
+      site_1: {
+        title: "Altar",
+        year: 2000,
+        era: "BC",
+        latitude: 51.513756,
+        longitude: -9.644037,
+        placemarkid: "->placemarks.schull"
+      }
+    }
   };
